55fed58463607aead45cfc1cda6086435b3d054c
rip <49995327+rip@users.noreply.github.com>
2019-12-30 19:00:53 -0500

    Create ogu.py

---
+++ ogu.py
@@ -0,0 +1,34 @@
+''' OGU AUTOBUMPER PROTOTYPE V.0
+may code a fully featured and cross-platform
+(run on any device or in the cloud!)
+version although this is functional...
+just a req l00p'''
+
+from time import sleep
+from request import post
+
+thread_ID_2bump = ''
+
+mybbuser = '' # auth cookie
+
+cookies = {'mybbuser': mybbuser}
+
+my_post_key = '' # found in request // todo: pull from cookie but this is just an example
+
+data = {
+'my_post_key': my_post_key, # ^
+'subject': '',
+'action': 'do_newreply',
+'posthash': '',
+'quoted_ids': '',
+'lastpid': '',
+'from_page': '1',
+'tid': thread_ID_2bump, # thread
+'method': 'quickreply',
+'message': '🦎🦎🦎🦎🦎🦎🦎🦎🦎🦎', # message
+'postoptions[signature]': '1' # toggle sig
+}
+
+while True:
+	x = requests.post('https://ogusers.com/newreply.php', cookies=cookies, data=data).text
+	sleep(3600)
