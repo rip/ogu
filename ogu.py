792ae56c5feaa8e9de4210dac21fab878e2cf9a5
rip <49995327+rip@users.noreply.github.com>
2019-12-31 21:47:34 -0500

    Update ogu.py

--- ogu.py
+++ ogu.py
@@ -16,14 +16,14 @@ cookies = {'mybbuser': mybbuser}
 my_post_key = '' # found in request // todo: pull from cookie but this is just an example
 
 data = {
-'my_post_key': my_post_key, # ^
+'my_post_key': my_post_key,
 'subject': '',
 'action': 'do_newreply',
 'posthash': '',
 'quoted_ids': '',
 'lastpid': '',
 'from_page': '1',
-'tid': thread_ID_2bump, # thread
+'tid': thread_ID_2bump, 
 'method': 'quickreply',
 'message': '🦎🦎🦎🦎🦎🦎🦎🦎🦎🦎', # message
 'postoptions[signature]': '1' # toggle sig
@@ -31,4 +31,4 @@ data = {
 
 while True:
 	post('https://ogusers.com/newreply.php', cookies=cookies, data=data)
-	sleep(3600)
+	sleep(3600) # bump every x seconds (default: 3600)
